9dde798a9396a44a8926d11f14111d6a6c803a46
perikobermio <erikbasanez@gmail.com>
2016-05-15 22:51:46 +0200

    fondue mobiten

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -18,7 +18,30 @@
 			frametime = SDL_GetTicks();
 			while(SDL_PollEvent(&event)){
 				if(event.type == SDL_QUIT) running  = false;
+				if(event.type == SDL_KEYDOWN) {
+					switch(event.key.keysym.sym) {
+						case SDLK_ESCAPE: running  = false; break;
+					}
+				}
+				/*if(event.type == SDL_MOUSEMOTION) {
+					switch(event.button.button) {
+						case SDL_BUTTON_MIDDLE: running  = false; break;
+					}
+				}*/
+				if(event.type == SDL_MOUSEBUTTONDOWN) {
+					switch(event.button.button) {
+						case SDL_BUTTON_LEFT: _render.moveBack(_render, event.button.x, event.button.y); break;
+					}
+				}
+				//if(event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_MAXIMIZED)  maximizekeran mobidek itzeko
+				
 			}
+			
+			SDL_RenderClear(_render.ren);
+            _render.renderBack(_render);
+            SDL_RenderPresent(_render.ren);
+			
+			
 			frametime = SDL_GetTicks() - frametime;
             if(frametime < 10)SDL_Delay(Uint32(10-frametime));
 		}
--- cpp/render.h
+++ cpp/render.h
@@ -3,15 +3,39 @@
 
 class render {
 	private:
-		int SCREEN_W = 500, SCREEN_H = 500;
+		int SCREEN_W = 1500, SCREEN_H = 800;
 	
-		SDL_Renderer *ren;
 		SDL_Window *screen;
+		
+		struct background {
+			SDL_Texture* img;
+			int x = 0, y = 0, w, h;
+		} camboya;
 	
 	public:
+		SDL_Renderer *ren;
+	
 		void createWindow() {
-			screen = SDL_CreateWindow("Commandos", 0, 0, SCREEN_W, SCREEN_H, SDL_WINDOW_SHOWN);
+			screen = SDL_CreateWindow("Commandos", 200, 200, SCREEN_W, SCREEN_H, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
             ren = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            
+            loadBack();
+		}
+		
+		void renderBack(render _render) {
+			SDL_Rect spr = {_render.camboya.x,_render.camboya.y,_render.SCREEN_W,_render.SCREEN_H};
+			SDL_Rect dst = {0,0,_render.SCREEN_W,_render.SCREEN_H};
+			SDL_RenderCopy(_render.ren, _render.camboya.img, &spr, &dst);
+		}
+		void moveBack(render &_render, int x, int y) {
+			_render.camboya.x = x;
+			_render.camboya.y = y;
+		}
+		
+		
+		
+		void maximizeScreen(render _render) {
+			SDL_MaximizeWindow(_render.screen);
 		}
 		
 		void destroyRender(render _render) {
@@ -21,6 +45,19 @@ class render {
             IMG_Quit();
             SDL_Quit();
         }
+       
+	private:
+		void loadBack() {
+			int w,h;
+			std::string _img = "../img/camboya.jpg";
+			SDL_Surface *img = IMG_Load(_img.c_str());
+            SDL_Texture *texture = SDL_CreateTextureFromSurface(ren, img);
+            SDL_QueryTexture(texture, NULL, NULL, &w, &h);
+            SDL_FreeSurface(img);
+            camboya.img = texture;
+            camboya.w = w;
+            camboya.h = h;
+		}
 };
 
 #endif
